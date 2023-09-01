b9d4748d580d52e84499a4a2b32e320e28e4f7f2
Salman <salmankps2001@gmail.com>
2023-09-01 23:42:55 +0530

    Added Order page and A simple loading.js file

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className='flex items-center justify-center w-[100vw] h-[100vh] bg-blue-50'>
+      <h1 className='text-center text-2xl font-bold'>
+        Loading..
+      </h1>
+    </div>
+  )
+}
+
+export default Loading;
