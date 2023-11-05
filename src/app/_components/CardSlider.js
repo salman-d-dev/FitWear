37a2928f9b2a06c5b639fddcd09a14c63699f35f
Salman <salmankps2001@gmail.com>
2023-11-05 20:05:24 +0530

    Made some slight changes

--- src/app/_components/CardSlider.js
+++ src/app/_components/CardSlider.js
@@ -54,10 +54,10 @@ const CardSlider = () => {
                 width={300} className="md:h-60 md:w-80 rounded-lg"
               />
             </div>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
+            <h2 className="text-lg text-gray-900 font-bold title-font mb-2">
               Express Your Style
             </h2>
-            <p className="leading-relaxed text-base">
+            <p className="leading-relaxed text-base text-center">
               Discover a world of creativity and self-expression with our
               exclusive collection of T-Shirts, Hoodies, Mugs, and Stickers.
               Elevate your wardrobe, add a touch of art to your daily routine,
@@ -65,25 +65,6 @@ const CardSlider = () => {
             </p>
           </div>
         
-          <div className="border-2 border-gray-900  rounded-lg shadow-lg cursor-pointer homeCard">
-            <div className=" mb-4">
-              <Image
-                src={"/darktshirt2.jpg"}
-                alt="TShirt.svg"
-                height={200}
-                width={300} className="md:h-60 md:w-80 rounded-lg"
-              />
-            </div>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-              Artistry Unleashed
-            </h2>
-            <p className="leading-relaxed text-base">
-              Our <Link href={"/tshirts"} className="text-blue-400">T-Shirts</Link> are more than just fabric; they are canvases for
-              self-expression. From bold and vibrant designs to intricate and
-              thought-provoking artwork, find the perfect t-shirt to showcase
-              your unique style.
-            </p>
-          </div>
         
           <div className="border-2 border-gray-900 rounded-lg shadow-lg cursor-pointer homeCard">
             <div className=" rounded-full mb-4">
@@ -95,10 +76,10 @@ const CardSlider = () => {
               />
               {/* /mug-svgrepo-com.svg */}
             </div>
-            <h2 className="text-lg font-medium title-font mb-2 text-gray-900">
+            <h2 className="text-lg font-bold title-font mb-2 text-gray-900">
               Mornings Made Better
             </h2>
-            <p className="leading-relaxed text-base">
+            <p className="leading-relaxed text-base text-center">
               Start your day right with our range of delightful <Link href={"/mugs"} className="text-blue-400">Mugs.</Link> Sip your
               morning coffee or tea in style with designs that brighten your
               day. Choose a mug that speaks to your mood and personality.
@@ -114,10 +95,10 @@ const CardSlider = () => {
                 width={300} className="md:h-60 md:w-80 rounded-lg"
               />
             </div>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
+            <h2 className="text-lg text-gray-900 font-bold title-font mb-2">
               Sticker Your World
             </h2>
-            <p className="leading-relaxed text-base">
+            <p className="leading-relaxed text-base text-center">
               Unleash your creativity with our unique <Link href={"/stickers"} className="text-blue-400">Sticker</Link> collection. From
               personalizing your laptop to adding a touch of fun to your space,
               our stickers let you make your mark on the world.
@@ -132,11 +113,30 @@ const CardSlider = () => {
                 width={300} className="md:h-60 md:w-80 rounded-lg"
               />
             </div>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
+            <h2 className="text-lg text-gray-900 font-bold title-font mb-2">
               Your Style Sanctuary
             </h2>
-            <p className="leading-relaxed text-base">
+            <p className="leading-relaxed text-base text-center">
               Welcome to your style sanctuary. From trendy <Link href={"/hoodies"} className="text-blue-400">Hoodies</Link> to cozy T-Shirts, artistic Mugs, and expressive Stickers, {`we've got everything you need to create a space that reflects your unique personality and style.`}
+            </p>
+          </div>
+          <div className="border-2 border-gray-900  rounded-lg shadow-lg cursor-pointer homeCard">
+            <div className=" mb-4">
+              <Image
+                src={"/darktshirt2.jpg"}
+                alt="TShirt.svg"
+                height={200}
+                width={300} className="md:h-60 md:w-80 rounded-lg"
+              />
+            </div>
+            <h2 className="text-lg text-gray-900 font-bold title-font mb-2">
+              Artistry Unleashed
+            </h2>
+            <p className="leading-relaxed text-base text-center">
+              Our <Link href={"/tshirts"} className="text-blue-400">T-Shirts</Link> are more than just fabric; they are canvases for
+              self-expression. From bold and vibrant designs to intricate and
+              thought-provoking artwork, find the perfect t-shirt to showcase
+              your unique style.
             </p>
           </div>
       </Slider>
